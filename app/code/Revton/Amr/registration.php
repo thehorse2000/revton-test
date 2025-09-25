12896572cd276b53bed699be448fc356afe6d5a9
Amr Osama <amrosama5@gmail.com>
2025-09-25 19:06:48 +0300

    Added new Theme and Module

---
+++ app/code/Revton/Amr/registration.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @category  Revton
+ * @package   Revton_Amr
+ */
+
+declare(strict_types=1);
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Revton_Amr',
+    __DIR__
+);
---
+++ app/design/frontend/Revton/Amr/registration.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @vendor      Revton
+ * @theme       Revton_Amr
+ * @author      Amr Osama <amrosama5@gmail.com>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Revton/Amr',
+    __DIR__
+);
